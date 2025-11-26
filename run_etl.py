d57a24be66f24236aacce76085d4d621d1094938
Andres Carmona <valdepeace@gmail.com>
2025-11-26 17:27:11 +0100

    test

---
+++ run_etl.py
@@ -0,0 +1,10 @@
+# etl/run_etl.py
+import time
+import os
+
+if __name__ == "__main__":
+    print("🏁 Iniciando ETL Ares (PoC)...")
+    time.sleep(2)
+    print("📦 Procesando datos...")
+    time.sleep(2)
+    print("✅ ETL Ares finalizado correctamente.")
