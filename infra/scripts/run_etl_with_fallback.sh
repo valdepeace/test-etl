26ab69f5925bce0ef990916f46243705f6972b7d
Andres Carmona <valdepeace@gmail.com>
2025-11-27 08:07:39 +0100

    .

--- infra/scripts/run_etl_with_fallback.sh
+++ infra/scripts/run_etl_with_fallback.sh
@@ -5,7 +5,7 @@ REPO_URL="${REPO_URL:?REPO_URL no definido}"
 BRANCH="${BRANCH:-main}"
 LOCAL_REPO="${LOCAL_REPO:-/opt/etl-repo}"
 
-LOG_DIR="/var/log/etl-ares"
+LOG_DIR="./etl-ares-logs"
 mkdir -p "$LOG_DIR"
 LOG_FILE="$LOG_DIR/etl-ares.log"
 
